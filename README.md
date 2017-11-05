89ea2521bdba3be47131128259b6cf043e94f14e
VsevolodKurochka <dramdoobas123456@gmail.com>
2017-11-05 13:03:16 +0200

    Update Readme.md and fixed task Build

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# gulp-start-template
-It's version for people who want to learn SASS
+# Gulp Starter template
+##### It's version for people who want to learn SASS
+___
+## How to install it?
+```
+cd YourFolderParent(example: C:\Users\Admin\Desktop\projects)
+git clone https://github.com/VsevolodKurochka/gulp-start-template.git
+cd gulp-start-template
+npm install
+```
+## Now, you should run the server:
+```
+gulp
+```
+## You can work!
--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
 // npm i gulpjs/gulp#4.0 browser-sync gulp-sass gulp-sass-glob gulp-autoprefixer
-const gulp = require('gulp');
-const install = require("gulp-install");
+const gulp                  = require('gulp');
+const install               = require("gulp-install");
+const browserSync           = require('browser-sync').create();
+const del                   = require('del');
+// CSS
+const sass                  = require('gulp-sass');
+const sassGlob              = require('gulp-sass-glob');
+const autoprefixer          = require('gulp-autoprefixer');
 
 // Installing all packages in package.json;
 gulp.src(['./package.json'])
   .pipe(install())
-
-const browserSync = require('browser-sync').create();
-
-// CSS
-const sass = require('gulp-sass');
-const sassGlob = require('gulp-sass-glob');
-const autoprefixer = require('gulp-autoprefixer');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', () =>
@@ -39,6 +38,38 @@ gulp.task('watch', () => {
   gulp.watch("app/sass/**/*.scss", gulp.series('sass'));
   gulp.watch("app/*.html").on('all', browserSync.reload);
 
+});
+
+// clean task
+gulp.task('clean', () => del.sync('dest') );
+
+// copyFiles
+// gulp.task('copyFiles', () => {
+//   gulp.src('app/css/**/*.css')
+//     .pipe(gulp.dest('dest/css'))
+
+//   gulp.src('app/img/**/*')
+//     .pipe(gulp.dest('dest/img'))
+
+//   gulp.src('app/*.html')
+//     .pipe(gulp.dest('dest'))
+
+//   gulp.src('app/js/**/*')
+//     .pipe(gulp.dest('dest/js'))
+// });
+
+gulp.task('build', gulp.series('sass'), function(){
+  gulp.src('app/css/**/*.css')
+    .pipe(gulp.dest('dest/css'))
+
+  gulp.src('app/img/**/*')
+    .pipe(gulp.dest('dest/img'))
+
+  gulp.src('app/*.html')
+    .pipe(gulp.dest('dest'))
+
+  gulp.src('app/js/**/*')
+    .pipe(gulp.dest('dest/js'))
 });
 
 gulp.task('default', gulp.series('watch'));
